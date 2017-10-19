e5423b2a5ef4a93743548511635a12298bbb4b4a
Andrey Safonov <andrey.safonov@scalefocus.com>
2017-10-19 17:34:12 +0300

    first commit

---
+++ webApiSelfHosted/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.HttpSys;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+
+namespace webApiSelfHosted
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).RunAsService();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(pathToContentRoot)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
